fdc7582e9c84d1bcafcf94b7198563ff890c43ca
Sabrey1 <limsabriey94@gmail.com>
2026-07-24 10:15:35 +0700

    update

--- contact_management/server/index.ts
+++ contact_management/server/index.ts
@@ -57,18 +57,53 @@
 // console.log("API running http://localhost:3000");
 
 
-import { Hono } from "hono";
-import { serve } from "@hono/node-server";
+// import { Hono } from "hono";
+// import { serve } from "@hono/node-server";
 
+// import contactRoutes from "./routes/contacts";
+
+// const app = new Hono();
+
+// app.route("/api/contacts", contactRoutes);
+
+// serve({
+//   fetch: app.fetch,
+//   port: 3000,
+// });
+
+// console.log("API running at http://localhost:3000");
+
+
+import { Hono } from "hono";
 import contactRoutes from "./routes/contacts";
 
-const app = new Hono();
+import type { D1Database } from "@cloudflare/workers-types";
 
-app.route("/api/contacts", contactRoutes);
 
-serve({
-  fetch: app.fetch,
-  port: 3000,
+type Bindings = {
+  DB: D1Database;
+};
+
+
+const app = new Hono<{
+  Bindings: Bindings;
+}>();
+
+
+app.get("/api/hello", (c)=>{
+
+  return c.json({
+    success:true,
+    message:"Contact API running"
+  });
+
 });
 
-console.log("API running at http://localhost:3000");
+
+app.route(
+  "/api/contacts",
+  contactRoutes
+);
+
+
+export default app;
